64948e1bf33708f9136ce0833b72d3009ef415d1
alex olivares <alexander.b.olivares@gmail.com>
2021-05-18 18:23:08 -0500

    guests_email col added to users table. register req had field for guests_email

--- database.sql
+++ database.sql
@@ -13,5 +13,6 @@ CREATE TABLE users(
   user_name VARCHAR(255) NOT NULL,
   user_email VARCHAR(255) NOT NULL UNIQUE,
   user_password  VARCHAR(255) NOT NULL,
+  guests_email VARCHAR(255),
   PRIMARY KEY (user_id)
 )
