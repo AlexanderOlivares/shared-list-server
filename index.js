c82def0513016e7bb7cad82a3b6050111b43e4bc
alex olivares <alexander.b.olivares@gmail.com>
2021-05-28 21:38:51 -0500

    reset password route created

--- index.js
+++ index.js
@@ -12,6 +12,27 @@ app.use("/auth", require("./routes/jwtAuth"));
 // dashboard route
 app.use("/dashboard", require("./routes/dashboard"));
 
+app.post("/resetpassword", async (req, res) => {
+  const { emailOfResetUser } = req.body;
+  try {
+    // check if user doesn't exist
+    console.log(emailOfResetUser);
+    const user = await pool.query(
+      "SELECT user_name, user_id FROM users WHERE user_email = $1",
+      [emailOfResetUser]
+    );
+
+    if (user.rows.length === 0) {
+      return res.status(401).json("No account found with this email address.");
+    }
+
+    console.log(user.rows);
+    res.json(user.rows);
+  } catch (err) {
+    res.status(500).send("Server Error");
+  }
+});
+
 app.listen(5000, () => {
   console.log("Listening on port 5000");
 });
